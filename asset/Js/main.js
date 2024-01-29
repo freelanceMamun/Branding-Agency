c1ad8142e6761420305b72847d81d86e75a439bb
FreelancerMamun <mamunkhan16345@gmail.com>
2024-01-30 00:16:14 +0600

    update

--- asset/Js/main.js
+++ asset/Js/main.js
@@ -1,56 +1,47 @@
 // ==========   Swiper Slider function
 
-var swiper = new Swiper('.mySwiper', {
-  spaceBetween: 30,
-  effect: 'fade',
-  loop: true,
-  auto: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+function SwiperCustom(className, loop, autoplay) {
+  new Swiper(`.${className}`, {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: loop,
+    auto: autoplay,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+    },
+  });
+}
+
+SwiperCustom('mySwiper', true, true);
+
+// var swiper = new Swiper('.mySwiper', {
+//   spaceBetween: 30,
+//   effect: 'fade',
+//   loop: true,
+//   auto: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+gsap.timeline().from('.md-title', {
+  y: 100,
+  opacity: 0,
+  duration: 1.2,
+  autoAlpha: 0,
+
+  stagger: function (index, target, list) {
+    // your custom logic here. Return the delay from the start (not between each)
+    return index * 0.5;
   },
 });
-
-// $(function () {
-//   var $startAnim = $('#start-anim');
-//   var $demoText = $('#demo-text');
-
-//   //Split the text into characters and wrap every character into span element, then convert the whitespaces to whitespace characters.
-//   $demoText.html(
-//     $demoText.html().replace(/./g, '<h1>$&</h1>').replace(/\s/g, '&nbsp;')
-//   );
-
-//   $startAnim.on('click', startAnimation);
-
-//   function startAnimation() {
-//     TweenLite.set($startAnim, {
-//       autoAlpha: 0,
-//     });
-
-//     //TweenMax.staggerFromTo( $demoText.find("span"), 0.4, {autoAlpha:0, scale:0}, {autoAlpha:1, scale:1}, 0.1, reset );
-
-//     TweenMax.staggerFromTo(
-//       $demoText.find('h1'),
-//       0.4,
-//       {
-//         autoAlpha: 0,
-//         scale: 7,
-//       },
-//       {
-//         autoAlpha: 1,
-//         scale: 1,
-//       },
-//       0.1
-//     );
-//   }
-
-// function reset() {
-//   TweenMax.to($startAnim, 1, {
-//     autoAlpha: 1,
-//   });
-// }
-// });

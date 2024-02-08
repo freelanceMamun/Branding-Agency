79f1fbe35c83890d0238a869b509c104e620fd71
FreelancerMamun <mamunkhan16345@gmail.com>
2024-02-08 22:27:19 +0600

    update

--- asset/Js/main.js
+++ asset/Js/main.js
@@ -33,3 +33,30 @@ SwiperCustom('mySwiper', true, true);
 //     clickable: true,
 //   },
 // });
+
+// ====== Infiniti text horizontal  Scroll
+const rows = document.querySelectorAll('.cb-tagreel-row');
+
+rows.forEach(function (e, i) {
+  let row_width = e.getBoundingClientRect().width;
+  let row_item_width = e.children[0].getBoundingClientRect().width;
+  let initial_offset = ((2 * row_item_width) / row_width) * 100 * -1;
+  let x_translation = initial_offset * -1;
+  // console.log(e.children[0].clientWidth);
+  console.log(x_translation);
+
+  gsap.set(e, {
+    xPercent: `${initial_offset}`,
+  });
+
+  let duration = 25 * (i + 1);
+
+  var tl = gsap.timeline();
+
+  tl.to(e, {
+    ease: 'none',
+    duration: duration,
+    xPercent: 0,
+    repeat: -1,
+  });
+});

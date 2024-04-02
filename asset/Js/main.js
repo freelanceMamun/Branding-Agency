0be55addb9f02185e78d97eecf6f6c2bc106b275
FreelancerMamun <mamunkhan16345@gmail.com>
2024-04-02 19:27:17 +0600

    update

--- asset/Js/main.js
+++ asset/Js/main.js
@@ -3,6 +3,7 @@
 function SwiperCustom(className, loop, autoplay) {
   new Swiper(`.${className}`, {
     spaceBetween: 30,
+
     effect: 'fade',
     loop: loop,
     auto: autoplay,
@@ -17,7 +18,9 @@ function SwiperCustom(className, loop, autoplay) {
   });
 }
 
-SwiperCustom('mySwiper', true, true);
+//SwiperCustom('mySwiper', true, true);
+
+//SwiperCustom('brand-slider', true, true);
 
 // var swiper = new Swiper('.mySwiper', {
 //   spaceBetween: 30,
@@ -35,46 +38,65 @@ SwiperCustom('mySwiper', true, true);
 // });
 
 // ====== Infiniti text horizontal  Scroll
-const rows = document.querySelectorAll('.cb-tagreel-row');
+function Infiniti() {
+  const rows = document.querySelectorAll('.cb-tagreel-row');
 
-rows.forEach(function (e, i) {
-  let row_width = e.getBoundingClientRect().width;
-  let row_item_width = e.children[0].getBoundingClientRect().width;
-  let initial_offset = ((2 * row_item_width) / row_width) * 100 * -1;
-  let x_translation = initial_offset * -1;
-  // console.log(e.children[0].clientWidth);
+  rows.forEach(function (e, i) {
+    let row_width = e.getBoundingClientRect().width;
+    let row_item_width = e.children[0].getBoundingClientRect().width;
+    let initial_offset = ((2 * row_item_width) / row_width) * 100 * -1;
+    let x_translation = initial_offset * -1;
+    // console.log(e.children[0].clientWidth);
 
-  gsap.set(e, {
-    xPercent: `${initial_offset}`,
+    gsap.set(e, {
+      xPercent: `${initial_offset}`,
+    });
+
+    let duration = 25 * (i + 1);
+
+    var tl = gsap.timeline();
+
+    tl.to(e, {
+      ease: 'none',
+      duration: duration,
+      xPercent: 0,
+      repeat: -1,
+    });
   });
-
-  let duration = 25 * (i + 1);
-
-  var tl = gsap.timeline();
-
-  tl.to(e, {
-    ease: 'none',
-    duration: duration,
-    xPercent: 0,
-    repeat: -1,
-  });
-});
+}
 
 // ========= Section hover
 
-const contactbutton = document.querySelector('.contact-button');
-const card = document.querySelector('.card');
-card.addEventListener('mouseenter', (e) => {
-  contactbutton.classList.add('active-button');
-  
-});
+function SectionHover() {
+  const contactbutton = document.querySelector('.contact-button');
+  const card = document.querySelector('.branding-banner-img');
 
-card.addEventListener('mouseleave', (e) => {
-  contactbutton.classList.remove('active-button');
-});
+  // card.addEventListener('mouseleave', (e) => {
+  //   contactbutton.classList.remove('active-button');
+  // });
 
-card.addEventListener('mousemove', function (e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  contactbutton.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)  `;
-});
+  card.addEventListener('mouseenter', () => {
+    console.log('eneter');
+    gsap.to(contactbutton, {
+      opacity: 1,
+      scale: 1,
+    });
+  });
+
+  card.addEventListener('mouseleave', () => {
+    gsap.to(contactbutton, {
+      opacity: 0,
+      scale: 0,
+    });
+  });
+
+  card.addEventListener('mousemove', function (e) {
+    // var x = e.clientX;
+    // var y = e.clientY;
+    // contactbutton.style.transform = `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)  `;
+    gsap.to(contactbutton, {
+      x: e.clientX - card.getBoundingClientRect().x - 50,
+      y: e.clientY - card.getBoundingClientRect().y - 100,
+    });
+  });
+}
